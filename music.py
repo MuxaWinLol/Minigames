1920853adf576a037f992638f4579968726e52bf
ubermensch1889 <71812335+ubermensch1889@users.noreply.github.com>
2021-03-01 19:55:01 +0300

    Update music.py
    Исправил зацикливание

--- music.py
+++ music.py
@@ -5,4 +5,4 @@ def play_music():
     pygame.mixer.music.load(r'data\sound_0.mp3')
     for i in range(6, 0, -1):
         pygame.mixer.music.queue(r'data\sound_{}.mp3'.format(i))
-    pygame.mixer.music.play(-1)
+    pygame.mixer.music.play()
