40e42c404cd8a2c58c2f515ddfd308a8534a6c15
ubermensch1889 <71812335+ubermensch1889@users.noreply.github.com>
2021-03-01 18:07:22 +0300

    Add files via upload

---
+++ forinarow.py
@@ -0,0 +1,280 @@
+import numpy as np
+import random
+import pygame
+import sys
+import math
+
+pygame.init()
+
+BLUE = pygame.Color('blue')
+BLACK = pygame.Color('black')
+RED = pygame.Color('red')
+YELLOW = pygame.Color('yellow')
+
+PLAYER = 0
+AI = 1
+
+EMPTY = 0
+PLAYER_PIECE = 1
+AI_PIECE = 2
+
+WINDOW_LENGTH = 4
+
+SQUARESIZE = 100
+
+RADIUS = int(SQUARESIZE / 2 - 5)
+
+
+class ForInARow:
+    def __init__(self, row_count, col_count):
+        self.row_count = row_count
+        self.col_count = col_count
+        self.width = col_count * SQUARESIZE
+        self.height = (row_count + 1) * SQUARESIZE
+        self.size = (self.width, self.height)
+        self.screen = pygame.display.set_mode(self.size)
+        self.board = self.create_board()
+        self.draw_board(self.board)
+
+    def create_board(self):
+        board = np.zeros((self.row_count, self.col_count))
+        return board
+
+    def drop_piece(self, board, row, col, piece):
+        board[row][col] = piece
+
+    def is_valid_location(self, board, col):
+        return board[self.row_count - 1][col] == 0
+
+    def get_next_open_row(self, board, col):
+        for r in range(self.row_count):
+            if board[r][col] == 0:
+                return r
+
+    def winning_move(self, board, piece):
+        for c in range(self.col_count - 3):
+            for r in range(self.row_count):
+                if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and \
+                        board[r][c + 3] == piece:
+                    return True
+
+        for c in range(self.col_count):
+            for r in range(self.row_count - 3):
+                if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and \
+                        board[r + 3][c] == piece:
+                    return True
+
+        for c in range(self.col_count - 3):
+            for r in range(self.row_count - 3):
+                if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][c + 3] == piece:
+                    return True
+
+        for c in range(self.col_count - 3):
+            for r in range(3, self.row_count):
+                if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
+                    return True
+
+    def evaluate_window(self, window, piece):
+        score = 0
+        opp_piece = PLAYER_PIECE
+        if piece == PLAYER_PIECE:
+            opp_piece = AI_PIECE
+
+        if window.count(piece) == 4:
+            score += 100
+        elif window.count(piece) == 3 and window.count(EMPTY) == 1:
+            score += 5
+        elif window.count(piece) == 2 and window.count(EMPTY) == 2:
+            score += 2
+
+        if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
+            score -= 4
+
+        return score
+
+    def score_position(self, board, piece):
+        score = 0
+
+        center_array = [int(i) for i in list(board[:, self.col_count // 2])]
+        center_count = center_array.count(piece)
+        score += center_count * 3
+
+        for r in range(self.row_count):
+            row_array = [int(i) for i in list(board[r, :])]
+            for c in range(self.col_count - 3):
+                window = row_array[c:c + WINDOW_LENGTH]
+                score += self.evaluate_window(window, piece)
+
+        for c in range(self.col_count):
+            col_array = [int(i) for i in list(board[:, c])]
+            for r in range(self.row_count - 3):
+                window = col_array[r:r + WINDOW_LENGTH]
+                score += self.evaluate_window(window, piece)
+
+        for r in range(self.row_count - 3):
+            for c in range(self.col_count - 3):
+                window = [board[r + i][c + i] for i in range(WINDOW_LENGTH)]
+                score += self.evaluate_window(window, piece)
+
+        for r in range(self.row_count - 3):
+            for c in range(self.col_count - 3):
+                window = [board[r + 3 - i][c + i] for i in range(WINDOW_LENGTH)]
+                score += self.evaluate_window(window, piece)
+
+        return score
+
+    def is_terminal_node(self, board):
+        return self.winning_move(board, PLAYER_PIECE) or \
+               self.winning_move(board, AI_PIECE) or len(self.get_valid_locations(board)) == 0
+
+    def minimax(self, board, depth, alpha, beta, maximizingPlayer):
+        valid_locations = self.get_valid_locations(board)
+        is_terminal = self.is_terminal_node(board)
+        if depth == 0 or is_terminal:
+            if is_terminal:
+                if self.winning_move(board, AI_PIECE):
+                    return None, 100000000000000
+                elif self.winning_move(board, PLAYER_PIECE):
+                    return None, -10000000000000
+                else:
+                    return None, 0
+            else:
+                return None, self.score_position(board, AI_PIECE)
+        if maximizingPlayer:
+            value = -math.inf
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = self.get_next_open_row(board, col)
+                b_copy = board.copy()
+                self.drop_piece(b_copy, row, col, AI_PIECE)
+                new_score = self.minimax(b_copy, depth - 1, alpha, beta, False)[1]
+                if new_score > value:
+                    value = new_score
+                    column = col
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    break
+            return column, value
+
+        else:
+            value = math.inf
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = self.get_next_open_row(board, col)
+                b_copy = board.copy()
+                self.drop_piece(b_copy, row, col, PLAYER_PIECE)
+                new_score = self.minimax(b_copy, depth - 1, alpha, beta, True)[1]
+                if new_score < value:
+                    value = new_score
+                    column = col
+                beta = min(beta, value)
+                if alpha >= beta:
+                    break
+            return column, value
+
+    def get_valid_locations(self, board):
+        valid_locations = []
+        for col in range(self.col_count):
+            if self.is_valid_location(board, col):
+                valid_locations.append(col)
+        return valid_locations
+
+    def pick_best_move(self, board, piece):
+        valid_locations = self.get_valid_locations(board)
+        best_score = -10000
+        best_col = random.choice(valid_locations)
+        for col in valid_locations:
+            row = self.get_next_open_row(board, col)
+            temp_board = board.copy()
+            self.drop_piece(temp_board, row, col, piece)
+            score = self.score_position(temp_board, piece)
+            if score > best_score:
+                best_score = score
+                best_col = col
+
+        return best_col
+
+    def draw_board(self, board):
+        for c in range(self.col_count):
+            for r in range(self.row_count):
+                pygame.draw.rect(self.screen, BLUE,
+                                 (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))
+                pygame.draw.circle(self.screen, BLACK, (int(c * SQUARESIZE + SQUARESIZE / 2),
+                                                   int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)),
+                                   RADIUS)
+
+        for c in range(self.col_count):
+            for r in range(self.row_count):
+                if board[r][c] == PLAYER_PIECE:
+                    pygame.draw.circle(self.screen, RED, (int(c * SQUARESIZE + SQUARESIZE / 2),
+                                                     self.height - int(r * SQUARESIZE + SQUARESIZE / 2)),
+                                       RADIUS)
+                elif board[r][c] == AI_PIECE:
+                    pygame.draw.circle(self.screen, YELLOW, (int(c * SQUARESIZE + SQUARESIZE / 2),
+                                                        self.height - int(r * SQUARESIZE + SQUARESIZE / 2)),
+                                       RADIUS)
+        pygame.display.update()
+
+    def start(self):
+        pygame.display.update()
+        game_over = False
+        myfont = pygame.font.SysFont("monospace", 75)
+
+        turn = random.randint(PLAYER, AI)
+
+        while not game_over:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+                if event.type == pygame.MOUSEMOTION:
+                    pygame.draw.rect(self.screen, BLACK, (0, 0, self.width, SQUARESIZE))
+                    posx = event.pos[0]
+                    if turn == PLAYER:
+                        pygame.draw.circle(self.screen, RED, (posx, int(SQUARESIZE / 2)), RADIUS)
+
+                pygame.display.update()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pygame.draw.rect(self.screen, BLACK, (0, 0, self.width, SQUARESIZE))
+                    if turn == PLAYER:
+                        posx = event.pos[0]
+                        col = int(math.floor(posx / SQUARESIZE))
+
+                        if self.is_valid_location(self.board, col):
+                            row = self.get_next_open_row(self.board, col)
+                            self.drop_piece(self.board, row, col, PLAYER_PIECE)
+
+                            if self.winning_move(self.board, PLAYER_PIECE):
+                                label = myfont.render("Красный победил!", True, RED)
+                                self.screen.blit(label, (40, 10))
+                                game_over = True
+
+                            turn += 1
+                            turn = turn % 2
+
+                            self.draw_board(self.board)
+
+            if turn == AI and not game_over:
+                col, minimax_score = self.minimax(self.board, 5, -math.inf, math.inf, True)
+
+                if self.is_valid_location(self.board, col):
+                    row = self.get_next_open_row(self.board, col)
+                    self.drop_piece(self.board, row, col, AI_PIECE)
+
+                    if self.winning_move(self.board, AI_PIECE):
+                        label = myfont.render("Желтый победил!", True, YELLOW)
+                        self.screen.blit(label, (40, 10))
+                        game_over = True
+
+                    self.draw_board(self.board)
+
+                    turn += 1
+                    turn = turn % 2
+
+            if game_over:
+                pygame.time.wait(3000)
+
+game = ForInARow(6, 7)
+game.start()
---
+++ tictactoe.py
@@ -0,0 +1,395 @@
+import pygame
+import sys
+import os
+import random
+import time
+
+pygame.init()
+EMPTY = -1
+PLAYER1 = 0
+PLAYER2 = 1
+
+
+FPS = 50
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+class TicTacToe:
+    class Digit(pygame.sprite.Sprite):
+        def __init__(self, coords, color, size, *groups):
+            super().__init__(*groups)
+            x, y = coords
+            self.color = color
+            self.size = size
+            self.image = pygame.transform.scale(load_image(self.color + "_0.png"), self.size)
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
+
+        def increase(self, digit):
+            self.image = pygame.transform.scale(load_image(self.color + "_" + digit + ".png"), self.size)
+
+    def __init__(self, cell_size, margin):
+        self.player_1 = 0
+        self.player_2 = 0
+        self.vs_computer = None
+        self.ai = 0
+        self.margin = margin
+        self.cell_size = cell_size
+        self.width = cell_size * 3 + margin * 2
+        self.height = self.width + self.width // 4
+        size = self.width, self.height
+        self.screen = pygame.display.set_mode(size)
+        pygame.display.set_caption("Крестики-нолики")
+        self.cells = [[-1 for _ in range(3)] for _ in range(3)]
+        self.move = 0
+        self.scores = {
+            1: 100,
+            0: -100,
+            'draw': 0
+        }
+        self.all_sprites = pygame.sprite.Group()
+        self.x_image = pygame.transform.scale(load_image("krestik.png"), (self.cell_size, self.cell_size))
+
+        self.o_image = pygame.transform.scale(load_image("nolik.png"), (self.cell_size, self.cell_size))
+
+        self.difficulty = None
+
+        self.score_group_1 = pygame.sprite.Group()
+        self.score_group_2 = pygame.sprite.Group()
+
+        score_size = self.width // 8, self.height // 7
+
+        self.score1_2 = self.Digit((self.width // 8 - score_size[0] // 2, 5 * self.height // 6), 'b', score_size, self.score_group_1)
+        self.score1_1 = self.Digit((self.width // 8 + score_size[0] // 2, 5 * self.height // 6), 'b', score_size, self.score_group_1)
+
+        self.score2_1 = self.Digit((self.width - self.width // 8 + score_size[0] // 2 - score_size[0], 5 * self.height // 6), 'r', score_size, self.score_group_2)
+        self.score2_2 = self.Digit((self.width - self.width // 8 - score_size[0] // 2 - score_size[0], 5 * self.height // 6), 'r', score_size, self.score_group_2)
+
+    def signal(self, result):
+        self.move = 0
+        self.all_sprites.empty()
+        self.cells = [[-1 for _ in range(3)] for _ in range(3)]
+        self.all_sprites.update()
+        self.screen.fill((0, 0, 0))
+
+        if result == -1:
+            fon_name = "draw.png"
+        elif result == 0:
+            fon_name = "o_win.png"
+            self.player_1 += 1
+            self.score1_1.increase(str(self.player_1).rjust(2, '0')[1])
+            self.score1_2.increase(str(self.player_1).rjust(2, '0')[0])
+            self.score_group_1.update(self.screen)
+        else:
+            fon_name = "x_win.png"
+            self.player_2 += 1
+            self.score2_1.increase(str(self.player_2).rjust(2, '0')[1])
+            self.score2_2.increase(str(self.player_2).rjust(2, '0')[0])
+            self.score_group_2.update(self.screen)
+
+        fon = pygame.transform.scale(load_image(fon_name), (self.width, self.height))
+
+        self.screen.blit(fon, (0, 0))
+        pygame.display.flip()
+        time.sleep(3)
+        self.screen.fill((0, 0, 0))
+        self.update()
+
+    def selection_mode(self, difficulty=True):
+
+        button_size = self.width // 4, 7 * self.width // 4 // 20
+
+        def on_button_1(pos):
+            x = pos[0]
+            y = pos[1]
+            if self.width // 2 - self.width // 8 < x < self.width // 2 + button_size[0] \
+                    and self.height // 2 - 100 < y < self.height // 2 - 100 + button_size[1]:
+                return True
+            return False
+
+        def on_button_2(pos):
+            x = pos[0]
+            y = pos[1]
+            if self.width // 2 - self.width // 8 < x < self.height // 2 + button_size[0] \
+                    and self.height // 2 < y < self.height // 2 + button_size[1]:
+                return True
+            return False
+
+        class PlayerButton(pygame.sprite.Sprite):
+            def __init__(self, coords, image, image_fon, *groups):
+                super().__init__(*groups)
+                x, y = coords
+                self.image = pygame.transform.scale(load_image(image), button_size)
+                self.image_normal = pygame.transform.scale(load_image(image), button_size)
+                self.image_fon = pygame.transform.scale(load_image(image_fon), button_size)
+                self.turn = True
+                self.rect = self.image.get_rect()
+                self.rect.x = x
+                self.rect.y = y
+
+            def update(self):
+                if self.turn:
+                    self.image = self.image_fon
+                    self.turn = False
+                else:
+                    self.image = self.image_normal
+                    self.turn = True
+
+        coords_1 = self.width // 2 - self.width // 8, self.height // 2 - 100
+        coords_2 = self.width // 2 - self.width // 8, self.height // 2
+
+        sprites_1 = pygame.sprite.Group()
+        if difficulty:
+            button1_name = "1pl.png"
+            button1_fon_name = "1pl_fon.png"
+            button2_name = "2pl.png"
+            button2_fon_name = "2pl_fon.png"
+            fon_name = "selection_mode.png"
+        else:
+            button1_name = "hard.png"
+            button1_fon_name = "hard_fon.png"
+            button2_name = "easy.png"
+            button2_fon_name = "easy_fon.png"
+            fon_name = "difficulty_selection.png"
+
+        fon = pygame.transform.scale(load_image(fon_name), (self.width, self.height))
+
+        PlayerButton(coords_1, button1_name, button1_fon_name, sprites_1)
+        sprites_2 = pygame.sprite.Group()
+        PlayerButton(coords_2, button2_name, button2_fon_name, sprites_2)
+        sprites_1_fon = True
+        sprites_2_fon = True
+        sprites_1.draw(self.screen)
+        sprites_2.draw(self.screen)
+
+        while True:
+            if on_button_1(pygame.mouse.get_pos()) and sprites_1_fon:
+                sprites_1_fon = False
+                sprites_1.update()
+            elif not on_button_1(pygame.mouse.get_pos()) and not sprites_1_fon:
+                sprites_1_fon = True
+                sprites_1.update()
+            if on_button_2(pygame.mouse.get_pos()) and sprites_2_fon:
+                sprites_2.update()
+                sprites_2_fon = False
+            elif not on_button_2(pygame.mouse.get_pos()) and not sprites_2_fon:
+                sprites_2_fon = True
+                sprites_2.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if on_button_1(event.pos):
+                        if difficulty:
+                            self.vs_computer = True
+                            self.selection_mode(False)
+                            return
+                        self.difficulty = 1
+                        self.screen.fill((0, 0, 0))
+                        return
+                    elif on_button_2(event.pos):
+                        if difficulty:
+                            self.vs_computer = False
+                            self.screen.fill((0, 0, 0))
+                            return
+                        self.difficulty = 0
+                        self.screen.fill((0, 0, 0))
+                        return
+
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(fon, (0, 0))
+            sprites_1.draw(self.screen)
+            sprites_2.draw(self.screen)
+
+            pygame.display.flip()
+
+    def update(self):
+        self.screen.fill((0, 0, 0))
+        self.all_sprites.draw(self.screen)
+        self.score_group_1.draw(self.screen)
+        self.score_group_2.draw(self.screen)
+        color = pygame.Color('white')
+        pygame.draw.rect(self.screen, color, (self.cell_size, 0, self.margin, self.height - self.width // 4))
+        pygame.draw.rect(self.screen, color, (2 * self.cell_size + self.margin, 0, self.margin, self.height - self.width // 4))
+        pygame.draw.rect(self.screen, color, (0, self.cell_size, self.width, self.margin))
+        pygame.draw.rect(self.screen, color, (0, 2 * self.cell_size + self.margin, self.width, self.margin))
+
+        self.all_sprites.draw(self.screen)
+
+        pygame.display.flip()
+
+    def get_random_cell(self):
+        while True:
+            x, y = random.randint(0, 2), random.randint(0, 2)
+            if self.cells[y][x] == -1:
+                return x, y
+
+    def make_move(self, mouse_pos, is_user_turn):
+        krestik = pygame.sprite.Sprite()
+        krestik.image = self.x_image
+        krestik.rect = krestik.image.get_rect()
+
+        nolik = pygame.sprite.Sprite()
+        nolik.image = self.o_image
+        nolik.rect = nolik.image.get_rect()
+
+        cell_x = (mouse_pos[0] - self.margin) // (self.cell_size + self.margin)
+        cell_y = (mouse_pos[1] - self.margin) // (self.cell_size + self.margin)
+        x = cell_x * self.cell_size + cell_x * self.margin
+        y = cell_y * self.cell_size + cell_y * self.margin
+        if self.cells[cell_y][cell_x] == -1:
+            if is_user_turn:
+                self.cells[cell_y][cell_x] = self.move % 2
+                if self.move % 2 == PLAYER2:
+                    krestik = pygame.sprite.Sprite()
+                    krestik.image = self.x_image
+                    krestik.rect = krestik.image.get_rect()
+                    krestik.rect.x = x
+                    krestik.rect.y = y
+                    self.all_sprites.add(krestik)
+                else:
+                    nolik = pygame.sprite.Sprite()
+                    nolik.image = self.o_image
+                    nolik.rect = nolik.image.get_rect()
+                    nolik.rect.x = x
+                    nolik.rect.y = y
+                    self.all_sprites.add(nolik)
+
+                self.move += 1
+                if self.move != 9 and self.vs_computer:
+                    self.make_move(mouse_pos, False)
+
+        if not is_user_turn:
+            if self.difficulty:
+                x, y = self.get_computer_position()
+            else:
+                x, y = self.get_random_cell()
+            self.cells[y][x] = PLAYER2
+
+            x = x * self.cell_size + x * self.margin
+            y = y * self.cell_size + y * self.margin
+            krestik.rect.x = x
+            krestik.rect.y = y
+            self.all_sprites.add(krestik)
+            self.move += 1
+
+        self.all_sprites.update()
+        self.update()
+        pygame.display.flip()
+        if self.is_win(0, self.cells):
+            self.signal(0)
+            return
+
+        elif self.is_win(1, self.cells):
+            self.signal(1)
+            return
+        elif self.move == 9:
+            self.signal(-1)
+            return
+
+    def minimax(self, board, depth, is_ai_turn):
+        if self.is_win(PLAYER1, board):
+            return self.scores[PLAYER1]
+        if self.is_win(PLAYER2, board):
+            return self.scores[PLAYER2]
+        if self.is_draw(board):
+            return self.scores['draw']
+
+        if is_ai_turn:
+            # выбираем ход который нам выгодней
+            best_score = - sys.maxsize
+            for y in range(3):
+                for x in range(3):
+                    if board[y][x] == EMPTY:
+                        board[y][x] = PLAYER2
+                        score = self.minimax(board, depth + 1, False)
+                        board[y][x] = EMPTY
+                        best_score = max(best_score, score)
+        else:
+            # противник выбирает ход который нам не выгоден
+            best_score = sys.maxsize
+            for y in range(3):
+                for x in range(3):
+                    if board[y][x] == -1:
+                        board[y][x] = PLAYER1
+                        score = self.minimax(board, depth + 1, True)
+                        board[y][x] = EMPTY
+                        best_score = min(best_score, score)
+        return best_score
+
+    def get_computer_position(self):
+        move = None
+        best_score = -sys.maxsize
+        board = [self.cells[y].copy() for y in range(3)]
+        for y in range(3):
+            for x in range(3):
+                if board[y][x] == EMPTY:
+                    board[y][x] = PLAYER2
+                    score = self.minimax(board, 0, False)
+                    board[y][x] = EMPTY
+                    if score > best_score:
+                        best_score = score
+                        move = x, y
+        return move
+
+    def is_win(self, char, field):
+        opponent_char = int(not bool(char))
+        # проверяем строки
+        for y in range(3):
+            if opponent_char not in field[y] and EMPTY not in field[y]:
+                return True
+
+        # проверяем колонки
+        for x in range(3):
+            col = [field[0][x], field[1][x], field[2][x]]
+            if opponent_char not in col and EMPTY not in col:
+                return True
+
+        # проверяем диагонали
+        diagonal = [field[0][0], field[1][1], field[2][2]]
+        if opponent_char not in diagonal and EMPTY not in diagonal:
+            return True
+        diagonal = [field[0][2], field[1][1], field[2][0]]
+        if opponent_char not in diagonal and EMPTY not in diagonal:
+            return True
+
+        return False
+
+    def is_draw(self, board):
+        count = 0
+        for y in range(3):
+            count += 1 if EMPTY in board[y] else 0
+        return count == 0
+
+
+players_count = 1
+running = True
+game = TicTacToe(250, 20)
+game.selection_mode()
+game.update()
+pygame.display.flip()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            game.make_move(event.pos, True)
+            game.screen.fill((0, 0, 0))
+pygame.quit()
